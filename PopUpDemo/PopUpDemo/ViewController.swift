53857497d97f9c352f179702410be096c787b10d
Raincle <yaweihong@icloud.com>
2015-05-01 22:19:34 +0800

    Init

---
+++ PopUpDemo/PopUpDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PopUpDemo
+//
+//  Created by 洪亞偉 on 15/5/1.
+//  Copyright (c) 2015年 Raincle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
